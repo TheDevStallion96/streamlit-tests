a7aeacf57128a33f6095a499330ae57c626df407
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2024-02-07 15:37:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,3 @@ age_range = st.selectbox(
 # Color picker for favorite color
 favorite_color = st.color_picker('Pick your favorite color', '#00f900')
 
-# Button to submit choices
-if st.button('Submit'):
-    st.write(f'Hello, {name}!')
-    st.write(f'Your age range is: {age_range}')
-    st.write(f'Your favorite color is: ', favorite_color)
-else:
-    st.write('Please input your details and press submit.')
-
